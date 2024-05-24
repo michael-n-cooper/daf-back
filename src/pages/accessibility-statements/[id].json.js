079d0907944581650500e570407fc6b08f7a3005
Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
2024-05-24 10:55:19 -0400

    start work on endpoints

---
+++ src/pages/accessibility-statements/[id].json.js
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+
+const usernames = ["Sarah", "Chris", "Yan", "Elian"]
+
+export const GET: APIRoute = ({ params, request }) => {
+  const id = params.id;
+  return new Response(
+    JSON.stringify({
+      name: usernames[id]
+    })
+  )
+}
+
+export async function getStaticPaths() { 
+	const module = await import('./ids.js');
+	const {ids} = module;
+	return (ids);
+}
