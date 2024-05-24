520c328cdd710008670e165450ebc16ec46d863a
Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
2024-05-24 10:55:07 -0400

    copy scripts from daf-front

---
+++ src/script/util.js
@@ -0,0 +1,3 @@
+export { findObjectByProperties, filterObjectByProperties, idFrag, compareStr, mdToHtml, isValidUrl, getOneProp, getFileData, escSparql } from './util-base.js';
+
+export const baseUri = import.meta.env.BASE_URL;
