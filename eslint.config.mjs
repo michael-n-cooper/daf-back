bb34560ed37bbabde82c1691d237b6393cc08087
Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
2024-09-23 17:52:19 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import {eslint, pluginJs} from "@eslint/js";
+import tseslint from 'typescript-eslint';
 
+export default tseslint.config({
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+  ],
+  languageOptions: {
+    globals: globals.browser
+  },
+  plugins: pluginJs.configs.recommended,
+  rules: {
+    "typescript-eslint/no-floating-promises": "warn",
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+  }
+});
+
